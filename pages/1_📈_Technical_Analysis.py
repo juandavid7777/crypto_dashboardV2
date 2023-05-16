35a63c7e04b10c11c4f57ec9b24f30325aea2b1c
juandavid7777 <juandavid7777@gmail.com>
2023-05-16 22:47:38 +0400

    Update 1_📈_Technical_Analysis.py

--- pages/1_📈_Technical_Analysis.py
+++ pages/1_📈_Technical_Analysis.py
@@ -16,20 +16,30 @@ strl.write("Have you found this useful? Consider donating - BTC: 3EbH7JPSTGqSzyK
 strl.markdown("""---""")
 strl.caption("Customized indicator powered by Glassnode")
 
+#Sets API general parameters
 aws_api_url = strl.secrets["aws_api_url"]
 api_key = strl.secrets["aws_api_token"]
+
+#Calls metadata
 metric = "Metadata"
 price_bool = False
 normalize_bool = False
-
-
 df_metadata = aws_crypto_api(aws_api_url, metric, price_bool, normalize_bool, api_key)
 
 strl.write(df_metadata)
 
-df_thresholds = pd.read_csv("thresholds.csv")
-df_meta = df_thresholds[df_thresholds["type"].isin(["Technical"])]
+#Calls all data
+metric = "All"
+price_bool = False
+normalize_bool = False
+df_data = aws_crypto_api(aws_api_url, metric, price_bool, normalize_bool, api_key)
 
+strl.write(df_metadata.head())
+
+#Filters metadata to select metrics
+df_meta = df_metadata[df_metadata["type"].isin(["Technical"])]
+
+#Plots selected metrics
 col_bounded, col_colored= strl.columns(2)
 
 with col_bounded:
