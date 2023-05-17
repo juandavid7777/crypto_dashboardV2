b84c70d3cd21c0bd8b121b5782281fca2b8f083f
juandavid7777 <juandavid7777@gmail.com>
2023-05-17 17:16:05 +0400

    Update 4_⚙️_Machine_Learning.py

--- pages/4_⚙️_Machine_Learning.py
+++ pages/4_⚙️_Machine_Learning.py
@@ -179,7 +179,9 @@ with col_MLinputs:
     
     model_type = strl.selectbox('Machine learning model type',
                                 ('Random Forest', "Decision tree", 'Support Vector Machine', 'K-NN', 'Naive Bayes', "Logistic regression"))
-    
+
+strl.write(df_classified)
+
 #Splits the data
 X_train, y_train, X_test, y_test, split_dates_list = ML_XY_dataselector(df_classified, selected_variables, "bull_bear", start_date, mid_date, end_date)
 
