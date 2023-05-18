44195c5d1aadc6bf7e2b7e919a68d990145f9bfa
juandavid7777 <juandavid7777@gmail.com>
2023-05-18 16:15:12 +0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -19,9 +19,7 @@ strl.set_page_config(layout="wide", page_title="Home - BTC: " + str(btc_price), 
 
 # Title
 strl.image("bitcoin_strip.png", use_column_width = True)
-strl.caption("Have you found this useful? Consider donating ₿: 3EbH7JPSTGqSzyKKAijgva1ffXaY6JWk34")
-badge(type="buymeacoffee", name="juandavid7E")
-badge(type="twitter", name="barbosa83")
+strl.caption("Have you found this useful? Consider donating ₿: 3EbH7JPSTGqSzyKKAijgva1ffXaY6JWk34"), badge(type="buymeacoffee", name="juandavid7E"), badge(type="twitter", name="barbosa83")
 
 # Summary
 strl.markdown("""---""")
