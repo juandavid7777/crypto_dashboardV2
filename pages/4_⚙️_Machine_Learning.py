5caef58bbb53fa3836fb2a615aa22c61a78a809d
juandavid7777 <juandavid7777@gmail.com>
2023-05-20 13:27:41 +0400

    sizing

--- pages/4_⚙️_Machine_Learning.py
+++ pages/4_⚙️_Machine_Learning.py
@@ -19,7 +19,7 @@ strl.image("ML.jpg", use_column_width = True)
 
 # Donations
 strl.write("Unlock the potential of data science and bitcoin analysis to create a brighter financial future for all. Stay tunned, and keep accumulating!")
-col1, col2, col3, col4 = strl.columns([1.2, 1.2, 4, 5.6])
+col1, col2, col3, col4 = strl.columns([1.5, 1.5, 5, 4])
 
 with col1:
     badge(type="twitter", name="barbosa83")
