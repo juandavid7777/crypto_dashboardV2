fff04aa9632f0a2bd85d3469d8d4a960180bda2b
juandavid7777 <juandavid7777@gmail.com>
2023-05-23 15:21:03 +0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -38,6 +38,9 @@ colored_header(label = "", description = "", color_name="yellow-80")
 strl.header("Market summary")
 strl.caption("Powered by CoinGecko and Python Analytics")
 
+strl.write("The selected oscillating metrics consist of two technical indicators derived purely from price movements, five on-chain indicators based on wallet movements within the blockchain, and two sentiment indicators utilizing information extracted from social media platforms. Future work will add new metrics that follow an oscillating cyclic pattern that relates to the Bitcoin repeating dynamics. Common cyclic peaks, valleys and mid-cycle areas can be identified in individual metrics, however, a convergence approach should be implemented to reduce the risk when classifying the market trend.")
+strl.caption("Bullet figures below show the current value of the metric and the cycle area where has been mapped. For a historic visualization of each metric, check the Technical, On-chain and Sentiment sections.")
+
 col_mcap, col_per, col_price = strl.columns(3)
 
 with col_mcap:
