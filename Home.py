dd8925c6bd0bcf301afff4b03039cb5cf2d4deb5
juandavid7777 <juandavid7777@gmail.com>
2023-05-23 14:30:57 +0400

    Update Home.py

--- Home.py
+++ Home.py
@@ -31,7 +31,7 @@ with col2:
 with col3:
     strl.caption("₿: 3EbH7JPSTGqSzyKKAijgva1ffXaY6JWk34")
 
-strl.write("Bitcoin, the pioneering cryptocurrency, has experienced notable price cycles since its inception. These cycles often exhibit patterns influenced by various factors. One approach to understanding and predicting these cycles is by utilizing the concept of Bitcoin halving. Building on this idea, a comprehensive analysis of nine metrics has been conducted, encompassing technical, on-chain, and sentiment indicators. By incorporating information from multiple sources, this data-driven methodology aims to identify repeating patterns and predict the market’s trend.")
+strl.write("Bitcoin, the pioneering cryptocurrency, has experienced notable price cycles since its inception. These cycles often exhibit patterns influenced by various factors. One approach to understand and predict these cycles is by utilizing the concept of Bitcoin halving which defines a repetitive fundamental change which results in a reiteration of certain market dynamics. Building on this idea, a comprehensive analysis of nine metrics has been conducted, encompassing technical, on-chain, and sentiment indicators. By incorporating information from multiple sources, we implement a data-driven Machine Learning methodology to identify repeating patterns and map the market’s trend within the cycle.")
 
 # Summary
 colored_header(label = "", description = "", color_name="yellow-80")
