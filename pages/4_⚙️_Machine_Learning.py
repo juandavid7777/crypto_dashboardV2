48d30a411768fdaeb431b954e146625ee4c5455d
juandavid7777 <juandavid7777@gmail.com>
2023-06-05 08:22:29 +0400

    Update 4_⚙️_Machine_Learning.py

--- pages/4_⚙️_Machine_Learning.py
+++ pages/4_⚙️_Machine_Learning.py
@@ -177,7 +177,7 @@ with col_MLgraphs:
                                         ["Neural Network",'Random Forest', "Decision Tree", 'Support Vector Machine', 'K-NN', 'Naive Bayes', "Logistic Regression", ],
                                         ["Neural Network",'Random Forest', 'Support Vector Machine', 'K-NN', "Logistic Regression", ])
         
-        conf_threshold = strl.number_input('Confidence threshold for acceptance (%)', min_value = 50, max_value = 100, value = 80, help = "Defines the value under which the vote is discarded due to reduced consensus in all the voting models. It defines an area where the algorithm accuracy is too low to take any action according to our chosen preferences.")
+        conf_threshold = strl.number_input('Confidence threshold for acceptance (%)', min_value = 50, max_value = 100, value = 85, help = "Defines the value under which the vote is discarded due to reduced consensus in all the voting models. It defines an area where the algorithm accuracy is too low to take any action according to our chosen preferences.")
 
         rolling_window = strl.number_input('Days in rolling voting window', min_value = 1, max_value = 90, value = 7, help = "Defines the time window where all the votes are averaged together. Bigger windows will show smoother data, but will suffer from a bigger time lag from the market events.")
         strl.caption("Number of votes on a single day is equal to the votes for all the models for each day in the rolling window: nos.models x nos.daysWindow = " + str(len(model_type_list)* rolling_window) + " votes.")
