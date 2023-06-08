e9196c2b1595300f7ea8d8fadcf5765ee0661191
juandavid7777 <juandavid7777@gmail.com>
2023-06-08 10:58:25 +0400

    Update 0_🗝️_Account.py

--- pages/0_🗝️_Account.py
+++ pages/0_🗝️_Account.py
@@ -16,6 +16,9 @@ authenticator = stauth.Authenticate(
 )
 
 strl.title('User account')
+if strl.session_state["authentication_status"] != True:
+    strl.warning("To access our exclusive content please register or login to your account.")
+
 name, authentication_status, username = authenticator.login('Login for advanced metrics access', 'main')   
     
 #Basic session rendering
@@ -24,9 +27,7 @@ if strl.session_state["authentication_status"]:
     strl.success(f' You are logged in as: *{strl.session_state["username"]}*', icon="✅")
     strl.success(f' Your  registered email is: *{config["credentials"]["usernames"][strl.session_state["username"]]["email"]}*', icon="📧")
 
-else:
-    strl.warning("To access our exclusive content please register or login to your account.")
-    
+else:   
     if strl.session_state["authentication_status"] is False:
         strl.error('Username/password is incorrect')
     else:
