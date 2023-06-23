7758e876f09703e4fd8a35945f1250e5a1fe4c8d
juandavid7777 <juandavid7777@gmail.com>
2023-06-23 16:38:38 +0400

    Update functions_auth.py

--- functions_auth.py
+++ functions_auth.py
@@ -196,6 +196,7 @@ def auth_disconnected(authenticator, config):
                     save_config(config)
                     
                 else:
+                    str.write(username_forgot_pw, config["credentials"]["usernames"].keys())
                     if username_forgot_pw in config["credentials"]["usernames"].keys(): 
                         strl.info('Valid username')
                     else:
