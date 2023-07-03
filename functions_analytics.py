61143e283ab6b40f52c199c88823743e11ddf477
juandavid7777 <juandavid7777@gmail.com>
2023-07-03 17:53:58 +0400

    analytics

--- functions_analytics.py
+++ functions_analytics.py
@@ -19,6 +19,14 @@ def inject_ga():
 
         gtag('config', 'G-JLYYFBB2ZD', { 'debug_mode':true });
     </script>
+
+    <!-- Google Tag Manager -->
+    <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+    new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+    j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    })(window,document,'script','dataLayer','GTM-TDTZRLS');</script>
+    <!-- End Google Tag Manager -->
     """
 
     # Insert the script in the head tag of the static template inside your virtual
