fd0eff5030cab79c28596dedcbd00c9ca211ae20
juandavid7777 <juandavid7777@gmail.com>
2023-07-14 11:41:49 +0400

    Update functions_visual.py

--- functions_visual.py
+++ functions_visual.py
@@ -8,3 +8,10 @@ def clean_image():
                 </style>
                 """
     strl.markdown(hide_streamlit_style, unsafe_allow_html=True) 
+    # Add custom CSS to hide the GitHub icon
+    hide_github_icon = """
+    #GithubIcon {
+    visibility: hidden;
+    }
+    """
+    strl.markdown(hide_github_icon, unsafe_allow_html=True)
